b5f7eb0b83ca24facd112a4a891fe3ca7959a4d7
sekul hassan <sekulhassan3031@gmail.com>
2024-03-24 12:38:11 +0600

    navbar css fix

--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -1,26 +1,55 @@
-import React from 'react';
-import {Container, Nav, Navbar} from "react-bootstrap";
-import {Link} from "react-router-dom";
+import React, { useEffect, useState } from 'react';
+import { Container, Nav, Navbar } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 function Menu(props) {
-    return (
-       <Container fluid="true">
-           <Navbar expand="md" className="navBackground px-4">
-               <Link to="/"><Navbar.Brand href="#"><img className="logoImg" src="https://recursiveeducation.com/static/media/black.4283bfd86d63fc6b59db.png" alt=""/></Navbar.Brand></Link>
-               <Navbar.Toggle aria-controls="navbarScroll" />
-               <Navbar.Collapse id="navbarScroll">
-                   <Nav className="me-auto"/>
-                   <Nav className="navbarScroll">
-                       <Nav.Link><Link  className="navLink" to="/">Home</Link></Nav.Link>
-                       <Nav.Link><Link  className="navLink" to="/about">About</Link></Nav.Link>
-                       <Nav.Link><Link  className="navLink" to="/projects">Projects</Link></Nav.Link>
-                       <Nav.Link><Link  className="navLink" to="/skills">Skills</Link></Nav.Link>
-                       <Nav.Link><Link  className="navLink" to="/resume">Resume</Link></Nav.Link>
-                   </Nav>
+    const [css, setCss] = useState({
+        navBackground: "navBackgroundScroll",
+        navbar: "navbarScroll",
+        navLink: "navLinkScroll",
+    });
 
-               </Navbar.Collapse>
-           </Navbar>
-       </Container>
+
+    useEffect(() => {
+        const handleScroll = () => {
+            if (window.scrollY > 1) {
+                setCss({
+                    navBackground: "navBackgroundScroll",
+                    navbar: "navbarScroll",
+                    navLink: "navLinkScroll",
+                });
+            }
+            else{
+                setCss({
+                    navBackground: "navBackground",
+                    navbar: "navbar",
+                    navLink: "navLink",
+                })
+            }
+        };
+        window.addEventListener('scroll', handleScroll);
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+
+    return (
+        <Container fluid="true">
+            <Navbar fixed="top" expand="md" className={`${css.navBackground} pr-lg-3`}>
+                <Link to="/"><Navbar.Brand><img className="logoImg ml-3" src="https://recursiveeducation.com/static/media/black.4283bfd86d63fc6b59db.png" alt="" /></Navbar.Brand></Link>
+                <Navbar.Toggle aria-controls="navbarScroll"/>
+                <Navbar.Collapse id="navbarScroll">
+                    <Nav className="me-auto" />
+                    <Nav className={css.navbar}>
+                        <Nav.Link><Link className={css.navLink} to="/">Home</Link></Nav.Link>
+                        <Nav.Link><Link className={css.navLink} to="/about">About</Link></Nav.Link>
+                        <Nav.Link><Link className={css.navLink} to="/projects">Projects</Link></Nav.Link>
+                        <Nav.Link><Link className={css.navLink} to="/skills">Skills</Link></Nav.Link>
+                        <Nav.Link><Link className={css.navLink} to="/resume">Resume</Link></Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+        </Container>
     );
 }
 
